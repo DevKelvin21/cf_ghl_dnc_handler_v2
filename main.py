89f341e90fda17643d6c9f371ebce1f579f5c383
Kelvin Rojas <kelvinrojas66@gmail.com>
2025-03-04 09:57:44 -0600

    Update campaign retrieval in prime_to_dnc function to use customData

--- main.py
+++ main.py
@@ -15,13 +15,13 @@ def prime_to_dnc(request: Request):
 
     # Get the phone number
     phone_number = request_json['phone'] if request_json else request_args['phone']
-    # campaign = request_json['campaign'] if request_json else request_args['campaign']
+    campaign = request_json['campaign'] if request_json else request_args['customData']['campaign']
     # Get the user and password
     user = os.getenv('VICI_USER')
     password = os.getenv('VICI_PASSWORD')
 
     # Initialize the VICI object
-    vici = VICI("Rowena", user, password)
+    vici = VICI(campaign, user, password)
 
     # Add the phone number to the DNC list
     response = vici.add_dnc(phone_number)
